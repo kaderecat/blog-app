09124e74556063c64308bdb2c6c68036598b75be
kaderecat <106696872+kaderecat@users.noreply.github.com>
2023-08-14 12:23:17 +0300

    made a footer

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -4,16 +4,22 @@ import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon'
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 
 @NgModule({
-  declarations: [NavbarComponent,],
+  declarations: [NavbarComponent,FooterComponent],
   imports: [
     CommonModule,
     MatIconModule,
     MatMenuModule,
-    RouterModule
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  exports : [NavbarComponent]
+  exports : [NavbarComponent, FooterComponent]
 })
 export class SharedModule { }
