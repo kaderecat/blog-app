bd9254db55e84cca52f1725fc5c761936f01ba14
kaderecat <106696872+kaderecat@users.noreply.github.com>
2023-08-15 22:25:44 +0300

    added routh guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,18 +10,22 @@ import { AddPostComponent } from './components/add-post/add-post.component';
 import { SinglePostComponent } from './components/single-post/single-post.component';
 import { EditPostComponent } from './components/edit-post/edit-post.component';
 import { MyPostsComponent } from './components/my-posts/my-posts.component';
+import { canActivate,redirectLoggedInTo,redirectUnauthorizedTo  } from '@angular/fire/auth-guard';
+
+const redirectToLogin = () => redirectUnauthorizedTo(['login'])
+const redirectToHome = () => redirectLoggedInTo(['/'])
 
 const routes: Routes = [
 {path : '' , component : HeroComponent},
 {path : 'home' , component : HomeComponent},
-{path : 'login' , component : LoginComponent},
-{path : 'register' , component : RegisterComponent},
-{path : 'profile' , component : ProfileComponent},
+{path : 'login' , component : LoginComponent,...canActivate(redirectToHome)},
+{path : 'register' , component : RegisterComponent,...canActivate(redirectToHome)},
+{path : 'profile' , component : ProfileComponent,...canActivate(redirectToLogin)},
 {path : 'posts' , component : PostsComponent},
-{path : 'add-post' , component : AddPostComponent},
+{path : 'add-post' , component : AddPostComponent,...canActivate(redirectToLogin)},
 {path : 'posts/:id' , component : SinglePostComponent},
-{path : 'edit/:id' , component : EditPostComponent},
-{path : 'my-posts' , component : MyPostsComponent},
+{path : 'edit/:id' , component : EditPostComponent,...canActivate(redirectToLogin)},
+{path : 'my-posts' , component : MyPostsComponent,...canActivate(redirectToLogin)},
 
 ];
 
